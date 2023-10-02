5b7403b2ae48b8407d83db918da7b736a0115a5b
@eco-intern <git-intern@econotec.com.bo>
2023-10-02 01:12:13 -0400

    Subiendo repository

--- activosfijos/activosfijos/src/main/java/com/grupod/activosfijos/moneda/MonedaRepository.java
+++ activosfijos/activosfijos/src/main/java/com/grupod/activosfijos/moneda/MonedaRepository.java
@@ -1,2 +1,7 @@
-package com.grupod.activosfijos.moneda;public interface MonedaRepository {
+package com.grupod.activosfijos.moneda;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MonedaRepository extends JpaRepository<MonedaEntity,Integer> {
+
 }
