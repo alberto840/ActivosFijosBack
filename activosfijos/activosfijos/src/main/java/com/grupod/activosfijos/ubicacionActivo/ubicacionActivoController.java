06c6a8dfd421753d00e649fc23a60b3fb195eb48
@eco-intern <git-intern@econotec.com.bo>
2023-10-02 01:17:13 -0400

    Subiendo controller

--- activosfijos/activosfijos/src/main/java/com/grupod/activosfijos/ubicacionActivo/ubicacionActivoController.java
+++ activosfijos/activosfijos/src/main/java/com/grupod/activosfijos/ubicacionActivo/ubicacionActivoController.java
@@ -1,2 +1,37 @@
-package com.grupod.activosfijos.ubicacionActivo;public class ubicacionActivoController {
+package com.grupod.activosfijos.ubicacionActivo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/ubicacion-activo")
+public class ubicacionActivoController {
+    private final ubicacionActivoService ubicacionActivoService;
+
+    @Autowired
+    public ubicacionActivoController(ubicacionActivoService ubicacionActivoService){
+        this.ubicacionActivoService = ubicacionActivoService;
+    }
+    @GetMapping("/all")
+    public List<ubicacionActivoEntity> getAllUbicacionActivos() {
+        return ubicacionActivoService.getAllUbicacionActivos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<ubicacionActivoEntity> getUbicacionActivoById(@PathVariable Integer id) {
+        return ubicacionActivoService.getUbicacionActivoById(id);
+    }
+
+    @PostMapping("/add")
+    public ubicacionActivoEntity addUbicacionActivo(@RequestBody ubicacionActivoEntity ubicacionActivo) {
+        return ubicacionActivoService.saveUbicacionActivo(ubicacionActivo);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUbicacionActivo(@PathVariable Integer id) {
+        ubicacionActivoService.deleteUbicacionActivo(id);
+    }
 }
