b727de903d60360140c59e24b64ce7479169de8a
@eco-intern <git-intern@econotec.com.bo>
2023-10-02 01:20:09 -0400

    subiendo service

--- activosfijos/activosfijos/src/main/java/com/grupod/activosfijos/ubicacionActivo/ubicacionActivoService.java
+++ activosfijos/activosfijos/src/main/java/com/grupod/activosfijos/ubicacionActivo/ubicacionActivoService.java
@@ -1,2 +1,33 @@
-package com.grupod.activosfijos.ubicacionActivo;public class ubicacionActivoService {
+package com.grupod.activosfijos.ubicacionActivo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ubicacionActivoService {
+    public final ubicacionActivoRepository ubicacionActivoRepository;
+
+    @Autowired
+    public ubicacionActivoService (ubicacionActivoRepository ubicacionActivoRepository){
+        this.ubicacionActivoRepository = ubicacionActivoRepository;
+    }
+
+    public List<ubicacionActivoEntity> getAllUbicacionActivos() {
+        return ubicacionActivoRepository.findAll();
+    }
+
+    public Optional<ubicacionActivoEntity> getUbicacionActivoById(Integer id) {
+        return ubicacionActivoRepository.findById(id);
+    }
+
+    public ubicacionActivoEntity saveUbicacionActivo(ubicacionActivoEntity ubicacionActivo) {
+        return ubicacionActivoRepository.save(ubicacionActivo);
+    }
+
+    public void deleteUbicacionActivo(Integer id) {
+        ubicacionActivoRepository.deleteById(id);
+    }
 }
